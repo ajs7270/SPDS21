7a79d2f20773d30578fbbf75f4ca85b4640bcd6c
jisuAn <ajs7270@naver.com>
2021-04-11 14:19:12 +0900

    Add sortable object to hw2 prob1

--- hw2/prob1.py
+++ hw2/prob1.py
@@ -6,6 +6,12 @@ class Schedule :
 
     def __str__(self):
         return self.name + ", " + self.start + ", " + self.end
+
+    def __lt__(self, other):
+        if self.start < other.start:
+            return True
+        else :
+            return False
      
 class Calendar :
     def __init__(self):
@@ -13,9 +19,16 @@ class Calendar :
 
     def __str__(self):
         result =""
-        for schedule in self.schedule_list :
-            result += str(schedule)
-        return result
+
+        self.schedule_list.sort()
+
+        for idx, schedule in enumerate(self.schedule_list) :
+            result += str(schedule) + '\n'
+            if idx == 9:
+                break
+            
+
+        return result 
 
     def _convert_time_to_int(self, time):
         day, hour = time.split()
@@ -29,21 +42,21 @@ class Calendar :
         return day, hour
             
     def _check_time(self, time):
+        # time type : "2021-04-05 09:35" , 
         target_day , target_time = self._convert_time_to_int(time)
 
         for schedule in self.schedule_list:
             start_day, start_time = self._convert_time_to_int(schedule.start)
             end_day, end_time = self._convert_time_to_int(schedule.end)
     
-            if (start_day <= target_day and target_day <= end_day):
-                if (start_time < target_time and target_time <= end_time):
+            if start_day <= target_day and target_day <= end_day:
+                if start_time < target_time and target_time <= end_time:
                     
                     return schedule
-
         return None
         
     def add_schedule(self, name, start, end):
-        if (self._check_time(start) == None and self._check_time(end) == None):
+        if self._check_time(start) == None and self._check_time(end) == None:
             self.schedule_list.append(Schedule(name,start,end))
         else :
             raise ValueError
@@ -88,4 +101,4 @@ if __name__ == '__main__':
     except ValueError:
         print("We have another schedule!")
 
-
+    print(calendar)
